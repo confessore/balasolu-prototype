d38234bb05ee5d89ccb5bb284061da81adf003aa
confessore <confessore@balasolu.com>
2022-10-23 17:52:04 -0400

    working on sendgrid integration

--- src/balasolu/pages/_app.tsx
+++ src/balasolu/pages/_app.tsx
@@ -2,6 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // move this to sendgrid.js
+  let fs = require('fs');
+  let key = fs.readFile(process.env.APPLICATION__SENDGRID__KEY);
+  console.log(key);
+  //
   return <Component {...pageProps} />
 }
 
