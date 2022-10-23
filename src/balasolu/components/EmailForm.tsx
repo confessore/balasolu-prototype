d17f18b417cc7a100d3ca8829f0f0eba3c96f6ab
confessore <confessore@balasolu.com>
2022-10-23 13:33:17 -0400

    binding onclick

--- src/balasolu/components/EmailForm.tsx
+++ src/balasolu/components/EmailForm.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component, FormEvent, useState } from 'react';
 import Bars3Icon from '@heroicons/react/24/outline/Bars3Icon';
 
 interface IProps {
@@ -21,6 +21,16 @@ export default class Service extends Component<IProps, IState> {
         }
     }
 
+    handleClick() {
+        console.log("hi");
+        const data = {
+            name: this.state.name,
+            email: this.state.email,
+            message: this.state.message
+        };
+        console.log(data);
+    };
+
     componentDidMount() {
 
     }
@@ -31,30 +41,30 @@ export default class Service extends Component<IProps, IState> {
 
     render() {
         return (
-            <div className="w-full h-auto md:w-[32rem] rounded-xl justify-center items-center m-1 p-8 bg-zinc-200 dark:bg-zinc-800 bg-opacity-90">
-                <div className="justify-between items-center m-1 p-1">
-                    <div className="text-lg font-medium justify-center items-center m-1 p-1">
-                        feel free to send us a message
-                    </div>
-                    <div className="flex justify-center items-center m-1 p-1">
-                        <div className="m-1 p-1">
-                            <div className="m-1 p-1 font-medium text-emerald-500 dark:text-emerald-400">what is your name?</div>
-                            <input id="name" type="text" onChange={e => this.setState({ name: e.target.value })} />
+                <div className="w-full h-auto md:w-[32rem] rounded-xl justify-center items-center m-1 p-8 bg-zinc-200 dark:bg-zinc-800 bg-opacity-90">
+                    <div className="justify-between items-center m-1 p-1">
+                        <div className="text-lg font-medium justify-center items-center m-1 p-1">
+                            feel free to send us a message
                         </div>
-                        <div className="m-1 p-1">
-                            <div className="m-1 p-1 font-medium text-emerald-500 dark:text-emerald-400">what is your email?</div>
-                            <input id="email" type="email" onChange={e => this.setState({ email: e.target.value })} />
+                        <div className="flex justify-center items-center m-1 p-1">
+                            <div className="m-1 p-1">
+                                <div className="m-1 p-1 font-medium text-emerald-500 dark:text-emerald-400">what is your name?</div>
+                                <input id="name" type="text" value={this.state.name} onChange={e => this.setState({ name: e.target.value })} />
+                            </div>
+                            <div className="m-1 p-1">
+                                <div className="m-1 p-1 font-medium text-emerald-500 dark:text-emerald-400">what is your email?</div>
+                                <input id="email" type="email" value={this.state.email} onChange={e => this.setState({ email: e.target.value })} />
+                            </div>
+                        </div>
+                        <div className="justify-center items-center m-1 p-1">
+                            <div className="m-1 p-1 font-medium text-emerald-500 dark:text-emerald-400">how may we assist you?</div>
+                            <textarea className="w-full" id="message" rows={4} value={this.state.message} onChange={e => this.setState({ message: e.target.value })} />
                         </div>
                     </div>
-                    <div className="justify-center items-center m-1 p-1">
-                        <div className="m-1 p-1 font-medium text-emerald-500 dark:text-emerald-400">how may we assist you?</div>
-                        <textarea className="w-full" id="message" rows={4} onChange={e => this.setState({ message: e.target.value })} />
+                    <div>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={this.handleClick.bind(this)}>send message</button>
                     </div>
                 </div>
-                <div>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">send message</button>
-                </div>
-            </div>
         );
     }
 }
