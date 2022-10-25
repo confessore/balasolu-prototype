97d9abd2cb27d8a5b520e98e7362c94f740103fc
confessore <confessore@balasolu.com>
2022-10-25 14:23:08 -0400

    spam prevention

--- src/balasolu/components/Emailer.tsx
+++ src/balasolu/components/Emailer.tsx
@@ -4,24 +4,31 @@ const Emailer = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const [valid, setValid] = useState(false);
     const [disabled, setDisabled] = useState(false);
+
+    const isValid = () => {
+        return (name.length !== 0 && email.length !== 0 && message.length !== 0);
+    }
 
     const handleClick = async () => {
         console.log('handling button click`');
-        setDisabled(true);
-        const host = window.location.protocol + "//" + window.location.host;
-        const url = host + '/api/sendEmail';
-        console.log(url);
-        await fetch(url, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ name: name, email: email, subject: 'email from contact form', description: message }),
-        });
-        setName('');
-        setEmail('');
-        setMessage('');
+        if (isValid()) {
+            setDisabled(true);
+            const host = window.location.protocol + "//" + window.location.host;
+            const url = host + '/api/sendEmail';
+            console.log(url);
+            await fetch(url, {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ name: name, email: email, subject: 'email from contact form', description: message }),
+            });
+            setName('');
+            setEmail('');
+            setMessage('');
+        }
         console.log('button click handled');
     }
 
@@ -33,19 +40,19 @@ const Emailer = () => {
                 </div>
                 <div className="m-1 p-1">
                     <div className="m-1 p-1 font-medium text-emerald-500 dark:text-emerald-400">what is your name?</div>
-                    <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                    <input id="name" type="text" disabled={disabled} value={name} onChange={(e) => { setName(e.target.value); setValid(isValid()); } } />
                 </div>
                 <div className="m-1 p-1">
                     <div className="m-1 p-1 font-medium text-emerald-500 dark:text-emerald-400">what is your email?</div>
-                    <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input id="email" type="email" disabled={disabled} value={email} onChange={(e) => { setEmail(e.target.value); setValid(isValid()); } } />
                 </div>
                 <div className="m-1 p-1">
                     <div className="m-1 p-1 font-medium text-emerald-500 dark:text-emerald-400">how may we assist you?</div>
-                    <textarea className="w-full" id="message" rows={4} value={message} onChange={(e) => setMessage(e.target.value)} />
+                    <textarea className="w-full" id="message" rows={4} disabled={disabled} value={message} onChange={(e) => { setMessage(e.target.value); setValid(isValid()); } } />
                 </div>
             </div>
             <div>
-                <button className="bg-blue-500 hover:bg-blue-700 disabled:bg-blue-300 disabled:hover:bg-blue-300 text-white font-bold py-2 px-4 rounded" disabled={disabled} onClick={() => handleClick()}>send message</button>
+                <button className="bg-blue-500 hover:bg-blue-700 disabled:bg-blue-300 disabled:hover:bg-blue-300 text-white font-bold py-2 px-4 rounded" disabled={disabled || !valid} onClick={() => handleClick()}>send message</button>
             </div>
         </div>
     );
